bc4b96b9b167f4c75185c74b4a2da439e86c5c94
Miguel-ectil <ectilmiguel@gmail.com>
2024-12-06 16:28:38 -0300

    [ADD] Adicionando novo exercício o número 12 dando desconto de 5% a um produto

---
+++ basics_sequences/exercício_12.js
@@ -0,0 +1,10 @@
+// ==== Desconto promocional ==== 
+
+
+let produto = 200
+
+const desconto = produto * 0.05
+precoPromocional = produto - desconto
+
+console.log('O preço do produto com desconto de 5% é ', { produto })
+
